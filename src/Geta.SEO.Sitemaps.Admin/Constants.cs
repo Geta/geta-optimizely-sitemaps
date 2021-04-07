0e9a9dcc675581d1e770da7474c64b551ed56bea
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-04-07 13:18:49 +0200

    feature: Add ServiceCollectionExtensions, MenuProvider, ModuleName constant

---
+++ src/Geta.SEO.Sitemaps.Admin/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Geta.SEO.Sitemaps.Admin
+{
+    public static class Constants
+    {
+        public const string ModuleName = "Geta.SEO.Sitemaps.Admin";
+    }
+}
---
+++ src/Geta.SEO.Sitemaps.Admin/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using EPiServer.DependencyInjection;
+using EPiServer.Shell.Modules;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Geta.SEO.Sitemaps.Admin
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSeoSitemaps(this IServiceCollection services)
+        {
+            services.AddCmsUI();
+            services.Configure<ProtectedModuleOptions>(
+                pm =>
+                {
+                    if (!pm.Items.Any(i => i.Name.Equals(Constants.ModuleName, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        pm.Items.Add(new ModuleDetails { Name = Constants.ModuleName});
+                    }
+                });
+
+            return services;
+        }
+    }
+}
