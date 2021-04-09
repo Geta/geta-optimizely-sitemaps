925279b318357cdac6a2907a7f9159de0b298ae9
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-04-09 15:27:20 +0200

    fix: Remove duplicate files and move proj file one level up

---
+++ src/Geta.SEO.Sitemaps.Admin/ContainerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Geta.SEO.Sitemaps.Admin
+{
+    public class ContainerController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
