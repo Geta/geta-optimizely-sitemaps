676d13daf4c440755397d73a082aff31d1888438
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-04-14 17:51:50 +0200

    feature: Add SitemapViewModel

---
+++ src/Geta.SEO.Sitemaps/Models/SitemapViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Geta.SEO.Sitemaps.Models
+{
+    public class SitemapViewModel
+    {
+        public string SiteUrl { get; set; }
+        public string LanguageBranche { get; set; }
+        public string Host { get; set; }
+        public bool EnableLanguageFallback { get; set; }
+        public bool IncludeAlternateLanguagePages { get; set; }
+        public bool EnableSimpleAddressSupport { get; set; }
+        public string PathsToAvoid { get; set; }
+        public string PathsToInclude { get; set; }
+        public bool IncludeDebugInfo { get; set; }
+        public string RootPageId { get; set; }
+        public string SitemapFormFormat { get; set; }
+    }
+}
