0904d717d5f5cdfd4a80f6a5e71ecd8e8def57b0
Jeff Fischer <Jeff.Fischer@episerver.com>
2022-10-04 11:40:03 -0400

    Removing period.

--- src/Geta.Optimizely.Sitemaps/Services/IUriAugmenterService.cs
+++ src/Geta.Optimizely.Sitemaps/Services/IUriAugmenterService.cs
@@ -9,7 +9,7 @@ namespace Geta.Optimizely.Sitemaps.Services
         /// <summary>
         /// Allows sitemap implementer an easy facility to take a simple url and expand it in a number of ways, includig parameterizing it with QueryStrings.
         /// </summary>
-        /// <param name="content">Original content of page URL being created.</param>
+        /// <param name="content">Original content of page URL being created</param>
         /// <param name="languageContentInfo">Language for URI</param>
         /// <param name="originUri">Origin URI to be included in sitemap</param>
         /// <returns>Must include origin to be included in sitemap</returns>
