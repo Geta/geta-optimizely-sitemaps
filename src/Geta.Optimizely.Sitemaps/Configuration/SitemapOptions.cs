dea737803935c277e425bfa12aed3594ff61cfe6
André Gabriel Coetzee <andre.gabriel.coetzee@gmail.com>
2024-11-26 20:21:07 +0200

    Update summaries

--- src/Geta.Optimizely.Sitemaps/Configuration/SitemapOptions.cs
+++ src/Geta.Optimizely.Sitemaps/Configuration/SitemapOptions.cs
@@ -11,13 +11,13 @@ namespace Geta.Optimizely.Sitemaps.Configuration
 
         /// <summary>
         /// The default is Mvc, this runs a check in the default content filter to ensure there's a page for every piece of content
-        /// Set this to headless if you are running a headless site to skip this check
+        /// Set this to headless if you are running a headless site to skip the check that ensures the content has an accompanying view
         /// </summary>
         public SiteArchitecture SiteArchitecture { get; set; } = SiteArchitecture.Mvc;
 
         /// <summary>
-        /// Enabled by default, this will, when using the default Content Filter, assume that content that can't be cast to IVersionable is unpublished
-        /// Consider disabling if you are finding that the default content filter is not generating content you're expecting for your sitemap
+        /// Enabled by default, this will, when using the default Content Filter, assume that any content that can't be cast to IVersionable is unpublished and not add it to the Sitemap
+        /// Consider disabling if you are finding that the default content filter is not generating content you're expecting to see in your sitemap
         /// </summary>
         public bool IsStrictPublishCheckingEnabled { get; set; } = true;
 
